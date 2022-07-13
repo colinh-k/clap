9ae835a089834e59cf761fc72842b1dac7932eb3
Colin Holzman-Klima <colinhk@icloud.com>
2022-07-13 11:29:11 -0700

    updated main for v2

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 
-#include "clapp.hpp"
+#include "clap.hpp"
 
 int main(int argc, char const *argv[]) {
     clap::CommandLineArgumentParser clap;
 
-    clap.addArg({"boolean", "b"});
-    clap.addArg({"fname", "f"});
-    clap.addArg({"range", "r"}, 3);
+    clap.addArg({"--boolean", "-b"});
+    clap.addArg({"--fname", "-f"});
+    clap.addArg({"range"}, 3);
     clap.parse(argc, argv);
 
     auto range = clap["range"];
     for (int i = 0; i < range.size(); i++) {
         std::cout << range[i] << "\n";
+    }
+
+    for (const auto& x : clap["boolean"]) {
+        std::cout << x << '\n';
+    }
+
+    for (const auto& x : clap["fname"]) {
+        std::cout << x << '\n';
     }
 
     std::cout << "done\n";
