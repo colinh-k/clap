1981d3cb6755cadf0a0968fda7d7b06b72cfeba2
Colin <colinh.k@icloud.com>
2022-07-19 17:28:22 -0700

    added exception classes; instead of exit on error, now throw exceptions

--- include/clap.hpp
+++ include/clap.hpp
@@ -32,6 +32,46 @@ namespace clap {
         return isShortFlag(name) || isLongFlag(name);
     }
 
+    // exception classes:
+    class ClapException : public std::runtime_error {
+        // base exception class
+        public:
+            ClapException(const std::string& message) :
+                std::runtime_error(message) {}
+    };
+
+    class ParseException : public ClapException {
+        // thrown when argument(s) are supplied in the
+        // wrong order, or omitted
+        public:
+            ParseException(const std::string& message) :
+                ClapException(message) {}
+    };
+
+    class TypeException : public ClapException {
+        // thrown when an argument cannot be interpreted
+        // as the required type
+        public:
+            TypeException(const std::string& message) :
+                ClapException(message) {}
+    };
+
+    class InvalidNameException : public ClapException {
+        // thrown when an invalid name is given, either
+        // to ArgumentParser::addArg, or ArgumentMap::operator[]
+        public:
+            InvalidNameException(const std::string& message) :
+                ClapException(message) {}
+    };
+
+    class HelpException : public ClapException {
+        // throws when the help flag is passed in the
+        // argument list (-h, --help)
+        public:
+            HelpException(const std::string& message) :
+                ClapException(message) {}
+    };
+
     // TODO: should this be a struct or class ?
     class ArgInfo {
         public:
@@ -113,7 +153,7 @@ namespace clap {
                 } else if (!isLongFlag(name) && nameToArg.count("--" + name)) {
                     return nameToArg["--" + name];
                 }
-                throw std::invalid_argument("argument name '" + name + "' does not exist");
+                throw InvalidNameException("argument name '" + name + "' does not exist");
             }
     }; // class ArgumentMap
 
@@ -130,17 +170,17 @@ namespace clap {
                         const std::size_t& nargs=1) {
                 // check for valid parameters
                 if (names.longName.empty()) {
-                    throw std::invalid_argument("a long name is required for every argument");
+                    throw InvalidNameException("a long name is required for every argument");
                 } else if (!isLongFlag(names.longName) && !(names.shortName.empty())) {
-                    throw std::invalid_argument("a positional argument may not have a short name");
+                    throw InvalidNameException("a positional argument may not have a short name");
                 } else if (!isShortFlag(names.shortName) && !names.shortName.empty()) {
-                    throw std::invalid_argument("invalid short name '" + names.shortName + "'. must have the form '-c', where 'c' can be any single character");
+                    throw InvalidNameException("invalid short name '" + names.shortName + "'. must have the form '-c', where 'c' can be any single character");
                 } else if (isShortFlag(names.longName)) {
-                    throw std::invalid_argument("long name '" + names.longName + "' may not be a short flag");
+                    throw InvalidNameException("long name '" + names.longName + "' may not be a short flag");
                 } else if (nameToArg.count(names.longName)) {
-                    throw std::invalid_argument("long name '" + names.longName + "' already exists");
+                    throw InvalidNameException("long name '" + names.longName + "' already exists");
                 } else if (nameToArg.count(names.shortName)) {
-                    throw std::invalid_argument("short name '" + names.shortName + "' already exists");
+                    throw InvalidNameException("short name '" + names.shortName + "' already exists");
                 }
 
                 // TODO: consider moving the isRequired logic from ArgInfo ctor
@@ -152,7 +192,10 @@ namespace clap {
                 // the map. this will prevent long names from being used with positional
                 // arguments ?
                 nameToArg[argInfo->longName] = argInfo;
-                nameToArg[argInfo->shortName] = argInfo;
+                if (!argInfo->shortName.empty()) {
+                    // do not add the empty string to the map
+                    nameToArg[argInfo->shortName] = argInfo;
+                }
                 // store a pointer to the argument info in order
                 // TODO: only do this if the name is supposed to be positional,
                 // i.e., only when no flag name is specified ?
@@ -163,90 +206,8 @@ namespace clap {
                 }
             }
 
-            ArgumentMap parse(int argc, char const **argv) {
-                if (!argc) {
-                    // TODO: should we accept 0 arguments when attempting to parse ?.
-                    // below, we assume at least one argument (the filename)
-                    // throw ParseException("No arguments availible to parse");
-                    // usage("<program name>");
-                    // error("no arguments availible to parse.");
-                    throw std::invalid_argument("no arguments available to parse");
-                }
-
-                // TODO: expect the first argument to be the filename;
-                fname = argv[0];
-                argv = &argv[1];
-                argc--;
-
-                // create the usage string once
-                makeUsage();
-
-                std::size_t posi = 0;  // index into positional arg list
-                std::size_t argi = 0;  // index into actual argument list
-                while (argi < argc) {
-                    const std::string arg{argv[argi]};
-
-                    if (arg == "-h" || arg == "--help") {
-                        // special help case
-                        help();
-                    }
-
-                    std::shared_ptr<ArgInfo> argInfo = nullptr;
-                    if (isFlag(arg)) {
-                        if (nameToArg.count(arg)) {
-                            argInfo = nameToArg[arg];
-                            argi++;  // consume this argument as a flag name
-                        } else {
-                            // invalid flag
-                            error("unrecognized flag '" + arg + "'");
-                        }
-                    } else {
-                        // non flag argument, so intepret it as the next positional argument
-                        if (posi < positionalArgs.size()) {
-                            argInfo = positionalArgs[posi];
-                            // update the current positional argument index
-                            posi++;
-                            // do not consume the current argument (by updating argi) because
-                            // we need to add it to the argument list below since it
-                            // is a positional argument
-                        } else {
-                            // if the index is out of bounds, report an error
-                            error("unexpected positional argument '" + arg + "'");
-                        }
-                    }
-
-                    for (std::size_t j = 0; j < argInfo->nargs; j++) {
-                        if (argi >= argc) {
-                            // index out of range for actual argument list
-                            error("expected " + std::to_string(argInfo->nargs) + " argument(s) for '" + argInfo->longName + "'");
-                        }
-
-                        const std::string argj{argv[argi]};
-
-                        if (argj == "-h" || argj == "--help") {
-                            // special help case
-                            help();
-                        }
-                        if (isFlag(argj)) {
-                            error("unexpected flag '" + argj + "' when parsing argument '" + argInfo->longName + "'");
-                        }
-                        // consume the next nargs arguments and store them
-                        // in the argInfo struct
-                        // increment
-                        argInfo->addValue(argj);
-                        argi++;
-                    }
-                }
-
-                // check if all the required arguments recieved a value
-                for (auto& pair : nameToArg) {
-                    if (pair.second->isRequired() && !(pair.second->hasValue)) {
-                        error("'" + pair.second->longName + "' is required");
-                    }
-                }
-
-                return ArgumentMap(std::move(nameToArg), std::move(usage));
-            }
+            ArgumentMap parse(int argc, char const **argv);
+            void help();
 
         private:
             // store names without dash prefixes as keys: e.g. f, filename
@@ -295,36 +256,114 @@ namespace clap {
                 }
                 return usage;
             }
-
-            void help() {
-                std::cout << usage << "\n";
-
-                if (!description.empty()) {
-                    std::cout << "Description:\n\t" << description << "\n\n";
-                }
-
-                std::cout << "Positional arguments:\n";
-                for (const auto& positionalArg : positionalArgs) {
-                    std::cout << "\t" << positionalArg->longName
-                              << "\t" << positionalArg->description << "\n";
-                }
-
-                std::cout << "\nOptional arguments:\n";
-                for (const auto& optionalArg : optionalArgs) {
-                    const std::string& names = (optionalArg->shortName.empty()) ?
-                                               optionalArg->longName :
-                                               optionalArg->shortName + ", " + optionalArg->longName;
-                    std::cout << "\t" << names
-                              << "\t" << optionalArg->description << "\n";
-                }
-                std::exit(EXIT_SUCCESS);
-            }
-
-            void error(const std::string& errorStr) {
-                // print the error string and exit with failure code
-                std::cout << usage;
-                std::cerr << "error: " << errorStr << std::endl;
-                std::exit(EXIT_FAILURE);
-            }
     };  // class ArgumentParser
+
+    ArgumentMap ArgumentParser::parse(int argc, char const **argv) {
+        if (!argc) {
+            // TODO: should we accept 0 arguments when attempting to parse ?.
+            // below, we assume at least one argument (the filename)
+            // throw ParseException("No arguments availible to parse");
+            // usage("<program name>");
+            throw InvalidNameException("no arguments available to parse");
+        }
+
+        // TODO: expect the first argument to be the filename;
+        fname = argv[0];
+        argv = &argv[1];
+        argc--;
+
+        // create the usage string once
+        makeUsage();
+
+        std::size_t posi = 0;  // index into positional arg list
+        std::size_t argi = 0;  // index into actual argument list
+        while (argi < argc) {
+            const std::string arg{argv[argi]};
+
+            if (arg == "-h" || arg == "--help") {
+                // special help case
+                // help();
+                throw HelpException("received help flag");
+            }
+
+            std::shared_ptr<ArgInfo> argInfo = nullptr;
+            if (isFlag(arg)) {
+                if (nameToArg.count(arg)) {
+                    argInfo = nameToArg[arg];
+                    argi++;  // consume this argument as a flag name
+                } else {
+                    // invalid flag
+                    throw ParseException("unrecognized flag '" + arg + "'");
+                }
+            } else {
+                // non flag argument, so intepret it as the next positional argument
+                if (posi < positionalArgs.size()) {
+                    argInfo = positionalArgs[posi];
+                    // update the current positional argument index
+                    posi++;
+                    // do not consume the current argument (by updating argi) because
+                    // we need to add it to the argument list below since it
+                    // is a positional argument
+                } else {
+                    // if the index is out of bounds, report an error
+                    throw ParseException("unexpected positional argument '" + arg + "'");
+                }
+            }
+
+            for (std::size_t j = 0; j < argInfo->nargs; j++) {
+                if (argi >= argc) {
+                    // index out of range for actual argument list
+                    throw ParseException("expected " + std::to_string(argInfo->nargs) + " argument(s) for '" + argInfo->longName + "'");
+                }
+
+                const std::string argj{argv[argi]};
+
+                if (argj == "-h" || argj == "--help") {
+                    // special help case
+                    // help();
+                    throw HelpException("received help flag");
+                }
+                if (isFlag(argj)) {
+                    throw ParseException("unexpected flag '" + argj + "' when parsing argument '" + argInfo->longName + "'");
+                }
+                // consume the next nargs arguments and store them
+                // in the argInfo struct
+                // increment
+                argInfo->addValue(argj);
+                argi++;
+            }
+        }
+
+        // check if all the required arguments recieved a value
+        for (auto& pair : nameToArg) {
+            if (pair.second->isRequired() && !(pair.second->hasValue)) {
+                throw ParseException("'" + pair.second->longName + "' is required");
+            }
+        }
+
+        return ArgumentMap(std::move(nameToArg), std::move(usage));
+    }
+
+    void ArgumentParser::help() {
+        std::cout << usage << "\n";
+
+        if (!description.empty()) {
+            std::cout << "Description:\n\t" << description << "\n\n";
+        }
+
+        std::cout << "Positional arguments:\n";
+        for (const auto& positionalArg : positionalArgs) {
+            std::cout << "\t" << positionalArg->longName
+                        << "\t" << positionalArg->description << "\n";
+        }
+
+        std::cout << "\nOptional arguments:\n";
+        for (const auto& optionalArg : optionalArgs) {
+            const std::string& names = (optionalArg->shortName.empty()) ?
+                                        optionalArg->longName :
+                                        optionalArg->shortName + ", " + optionalArg->longName;
+            std::cout << "\t" << names
+                        << "\t" << optionalArg->description << "\n";
+        }
+    }
 };  // namespace clap
