d354b0c2e502532045c7991707a361cb8d9b8fe9
Colin <colinh.k@icloud.com>
2022-07-28 15:25:15 -0700

    updated to new interface

--- src/main.cpp
+++ src/main.cpp
@@ -15,53 +15,21 @@ struct is_cont<std::vector<T>> {
     static const bool value = true;
 };
 
-// template <typename T>
-// struct identity { typedef T type; };
-
-// identity<decltype(mySet)>::type::value_type pos;
-
-// template <typename T>
-// void fromString(const std::string& str, T& value) {
-//     if (is_cont<T>::value) {
-//         typename T::value_type tv;
-//         std::stringstream stream;
-//         stream << str;
-//         stream >> tv;
-//         char c;
-//         if (stream.fail() || stream.get(c)) {
-//             // not an integer
-//             // return false;
-//             throw std::invalid_argument("not correct type");
-//         }
-//         value.insert(value.iterator(), tv);
-//     } else {
-//         std::stringstream stream;
-//         stream << str;
-//         stream >> value;
-//         char c;
-//         if (stream.fail() || stream.get(c)) {
-//             // not an integer
-//             // return false;
-//             throw std::invalid_argument("not correct type");
-//         }
-//     }
-// }
-
 int main() {
     // an example program to demonstrate the library
     clap::ArgumentParser parser;
 
-    parser.addArg({"--source", "-s"}, "a source file", clap::makeValueType<std::string>());
+    parser.addArg({"--source", "-s"}, "a source file", clap::makeValueType<std::vector<std::string>>(), 5);
     parser.addArg({"--destination", "-d"}, "a destination file", clap::makeValueType<std::string>());
     parser.addArg({"fname"}, "a filename", clap::makeValueType<std::string>());
-    parser.addArg({"--range"}, "specify range bounds", clap::makeValueType<std::string>());
+    parser.addArg({"--range"}, "specify range bounds", clap::makeValueType<int>());
 
-    std::size_t argc = 6;
+    std::size_t argc = 10;
     // char const *argv[] = {"program_name", "--help"};
     char const *argv[] = {"program_name", "file.txt",
-                          "--source", "src.txt",
+                          "--source", "src.txt", "src2.txt", "src3.txt", "src4.txt", "src5.txt",
                         //   "-d", "dest.txt",
-                          "--range", "5-10"};
+                          "--range", "5"};
     clap::ArgumentMap map;
     try {
         map = parser.parse(argc, argv);
@@ -69,19 +37,23 @@ int main() {
         // help flag was supplied
         std::cout << parser.help();
         return EXIT_SUCCESS;
-    } catch (clap::ParseException& e) {
+    } catch (clap::ClapException& e) {
         // parsing failed
         std::cerr << e.what() << '\n';
         std::cerr << parser.getUsage() << '\n';
         return EXIT_FAILURE;
     }
 
-    std::string source = map["s"].as<std::string>();
-    std::string fname = map["fname"].as<std::string>();
-    std::string range1 = map["range"].as<std::string>();
+    std::vector<std::string> source = map.get<std::vector<std::string>>("s");
+    std::string fname = map.get<std::string>("fname");
+    int range1 = map.get<int>("range");
 
     // std::cout << "destination: " << dest << '\n';
-    std::cout << "source: " << source << '\n';
+    // std::cout << "source: " << source << '\n';
+    for (auto& s : source) {
+        std::cout << s << " ";
+    }
+    std::cout << '\n';
     std::cout << "fname: " << fname << '\n';
     std::cout << "range1: " << range1 << '\n';
 
