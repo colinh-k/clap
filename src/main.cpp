657e88075d515a0ffa2c8615f276bc180a50aaec
Colin Holzman-Klima <colinhk@icloud.com>
2022-07-31 18:32:17 -0700

    added practical demonstration of parser

--- src/main.cpp
+++ src/main.cpp
@@ -19,16 +19,68 @@ int main() {
     // an example program to demonstrate the library
     clap::ArgumentParser parser;
 
-    parser.addArg({"--source", "-s"}, "a source file", clap::makeValueType<std::vector<std::string>>(), 5);
-    parser.addArg({"--destination", "-d"}, "a destination file", clap::makeValueType<std::string>());
-    parser.addArg({"fname"}, "a filename", clap::makeValueType<std::string>());
-    parser.addArg({"--range"}, "specify range bounds", clap::makeValueType<int>());
+    parser.addArg({"operation"}, "specify the type of operation to perform {encrypt or decrypt}", clap::Type<std::string>({"encrypt", "decrypt"}));
+    parser.addArg({"--input", "-i"}, "the input file", clap::Type<std::string>());
+    parser.addArg({"--output", "-o"}, "the output filename", clap::Type<std::string>());
+    parser.addArg({"--key", "-k"}, "the key file", clap::Type<std::string>());
+    std::vector<int> s = {0, 10};
+    parser.addArg({"--range", "-r"}, "range for operation {first-byte last-byte", clap::Type<std::vector<int>>(s), 2);
 
-    std::size_t argc = 10;
+    std::size_t argc = 11;
+    // char const *argv[] = {"program_name", "--help"};
+    char const *argv[] = {"./utility", "encrypt",
+                          "-i", "input.txt",
+                          "-o", "output.txt",
+                          "-k", "key.txt",
+                          "--range", "5", "6"};
+
+    clap::ArgumentMap map;
+    try {
+        map = parser.parse(argc, argv);
+    } catch (clap::HelpException& e) {
+        // help flag was supplied
+        std::cout << parser.help();
+        return EXIT_SUCCESS;
+    } catch (clap::ClapException& e) {
+        // parsing failed
+        std::cerr << e.what() << '\n';
+        std::cerr << parser.getUsage() << '\n';
+        return EXIT_FAILURE;
+    }
+
+    std::string op = map.get<std::string>("operation");
+    std::cout << "operation: " << op << '\n';
+
+    std::string input = map.get<std::string>("input");
+    std::cout << "input: " << input << '\n';
+
+    std::string output = map.get<std::string>("output");
+    std::cout << "output: " << output << '\n';
+    
+    std::string key = map.get<std::string>("key");
+    std::cout << "key: " << key << '\n';
+    
+    std::vector<int> range = map.get<std::vector<int>>("range");
+    std::cout << "range: " << range[0] << ' ' << range[1] << '\n';
+
+    return EXIT_SUCCESS;
+}
+
+/*
+int main() {
+    // an example program to demonstrate the library
+    clap::ArgumentParser parser;
+
+    parser.addArg({"--source", "-s"}, "a source file", clap::Type<std::vector<std::string>>(), 5);
+    parser.addArg({"--destination", "-d"}, "a destination file", clap::Type<std::string>("default"));
+    parser.addArg({"verbosity"}, "a required verbosity option", clap::Type<std::string>({"high", "low", "off"}));
+    parser.addArg({"fname"}, "a filename", clap::Type<std::string>());
+    parser.addArg({"--range"}, "specify range bounds", clap::Type<int>());
+
+    std::size_t argc = 11;
     // char const *argv[] = {"program_name", "--help"};
     char const *argv[] = {"program_name", "file.txt",
                           "--source", "src.txt", "src2.txt", "src3.txt", "src4.txt", "src5.txt",
-                        //   "-d", "dest.txt",
                           "--range", "5"};
     clap::ArgumentMap map;
     try {
@@ -47,8 +99,11 @@ int main() {
     std::vector<std::string> source = map.get<std::vector<std::string>>("s");
     std::string fname = map.get<std::string>("fname");
     int range1 = map.get<int>("range");
+    std::string dest = map.get<std::string>("d");
+    std::string verb = map.get<std::string>("verbosity");
 
-    // std::cout << "destination: " << dest << '\n';
+    std::cout << "destination: " << dest << '\n';
+    std::cout << "verbosity: " << verb << '\n';
     // std::cout << "source: " << source << '\n';
     for (auto& s : source) {
         std::cout << s << " ";
@@ -59,3 +114,4 @@ int main() {
 
     return 0;
 }
+*/
